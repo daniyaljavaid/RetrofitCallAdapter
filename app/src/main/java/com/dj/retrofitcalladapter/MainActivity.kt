7ed55a22661b9a7f2121138a11d8c67c600f38f2
daniyaljavaid <daniyaljavaid95@gmail.com>
2023-03-19 22:12:22 +0500

    different return types added for service

--- app/src/main/java/com/dj/retrofitcalladapter/MainActivity.kt
+++ app/src/main/java/com/dj/retrofitcalladapter/MainActivity.kt
@@ -7,26 +7,74 @@ import com.dj.retrofitcalladapter.common.ResultState
 import com.dj.retrofitcalladapter.datasource.service.ServiceObject
 import com.dj.retrofitcalladapter.datasource.service.ServiceObject.getServiceObject
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val apiService by lazy {
+        getServiceObject()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         callApi()
+        callApiWithResultState()
+        callApiWithResultStateFlow()
     }
 
     private fun callApi() {
         GlobalScope.launch {
             try {
-                when (val response = getServiceObject().getData()) {
-                    is ResultState.Success -> Log.d("----->", response.data.name)
-                    is ResultState.Error -> Log.d("----->", "Network Error")
-                }
+                val response = apiService.getData()
+                Log.d("callApi----->", response.name)
             } catch (exception: Exception) {
-                Log.e("----->", exception.toString())
+                Log.e("callApi----->", exception.toString())
             }
 
+        }
+    }
+
+    private fun callApiWithResultState() {
+        GlobalScope.launch {
+            try {
+                when (val response = apiService.getDataWithResultState()) {
+                    is ResultState.Success -> Log.d(
+                        "callApiWithResultState----->",
+                        response.data.name
+                    )
+                    is ResultState.Error -> Log.d("callApiWithResultState----->", "Network Error")
+                }
+            } catch (exception: Exception) {
+                Log.e("callApiWithResultState----->", exception.toString())
+            }
+        }
+    }
+
+    private fun callApiWithResultStateFlow() {
+        GlobalScope.launch {
+            try { // to catch exception if flow is not allowed in factory
+                apiService.getDataWithResultStateFlow()
+                    .catch { exception ->
+                        Log.e("callApiWithResultStateFlow----->", exception.toString())
+                    }
+                    .collect {
+                        when (it) {
+                            is ResultState.Success -> Log.d(
+                                "callApiWithResultStateFlow----->",
+                                it.data.name
+                            )
+                            is ResultState.Error -> Log.d(
+                                "callApiWithResultStateFlow----->",
+                                "Network Error"
+                            )
+                        }
+                    }
+            } catch (exception: Exception) {
+                Log.e("callApiWithResultStateFlow----->", exception.toString())
+            }
         }
     }
 }
--- app/src/main/java/com/dj/retrofitcalladapter/datasource/service/ApiService.kt
+++ app/src/main/java/com/dj/retrofitcalladapter/datasource/service/ApiService.kt
@@ -2,9 +2,16 @@ package com.dj.retrofitcalladapter.datasource.service
 
 import com.dj.retrofitcalladapter.common.ResultState
 import com.dj.retrofitcalladapter.dto.ResponseDto
+import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 
 interface ApiService {
     @GET("?name=dj")
-    suspend fun getData(): ResultState<ResponseDto>
+    suspend fun getData(): ResponseDto
+
+    @GET("?name=dj")
+    suspend fun getDataWithResultState(): ResultState<ResponseDto>
+
+    @GET("?name=dj")
+    suspend fun getDataWithResultStateFlow(): Flow<ResultState<ResponseDto>>
 }
